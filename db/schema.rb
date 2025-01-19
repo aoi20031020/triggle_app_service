2e50b7fc292307b34dad93d996f76a18784a1401
aoi20031020 <aoi20031020@gmail.com>
2025-01-19 21:38:37 +0900

    色々修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_01_19_103332) do
-  create_table "game_results", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.datetime "timestamp"
-    t.integer "player_number"
-    t.json "final_scores"
-    t.json "game_record"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "logs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "message"
     t.string "level"
